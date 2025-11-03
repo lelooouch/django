d76639684f37ac7b9f603e9f89a0f16c3c746a6c
lelooouch <118593450+lelooouch@users.noreply.github.com>
2025-11-03 19:12:44 +0500

    Update views.py

--- django_prjct/pythonProject6/starwars/peple/views.py
+++ django_prjct/pythonProject6/starwars/peple/views.py
@@ -4,10 +4,43 @@ import requests
 
 
 # Create your views here.
-
-def get_luke_info(request):
-    response = requests.get('https://swapi.dev/api/people/1/')
+def send_req(url):
+    response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
-        return render(request, 'peple/person.html', data)
+        return data
+    return None
+
+def update_data(data):
+    films = data['films']
+    vehicles = data['vehicles']
+    starships = data['starships']
+
+    new_films = list()
+    for film in films:
+        film = send_req(film)
+        new_films.append(film['title'])
+
+    new_vehicles = list()
+    for vehicle in vehicles:
+        vehicle = send_req(vehicle)
+        new_vehicles.append(vehicle['name'])
+
+    new_starships = list()
+    for starship in starships:
+        starship = send_req(starship)
+        new_starships.append(starship['name'])
+
+    data['films'] = new_films
+    data['vehicles'] = new_vehicles
+    data['starships'] = new_starships
+
+    return data
+
+def get_luke_info(request):
+    data = send_req('https://swapi.dev/api/people/1/')
+    if data:
+        updated_data = update_data(data)
+
+        return render(request, 'peple/person.html', updated_data)
     return HttpResponse('не удалось')

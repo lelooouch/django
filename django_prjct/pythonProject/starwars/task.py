f6a53eecaf15732bebbc38baeb19436206cfab99
lelooouch <118593450+lelooouch@users.noreply.github.com>
2025-11-17 16:32:20 +0500

    Add files via upload

--- django_prjct/pythonProject/starwars/task.py
+++ django_prjct/pythonProject/starwars/task.py
@@ -1,7 +1,5 @@
 import requests
 import json
-
-
 def send_req(url):
     response = requests.get(url)
     if response.status_code == 200:
@@ -10,8 +8,8 @@ def send_req(url):
 
 def parse():
     data_list = list()
-    for i in range(1, 83):
-        data = send_req(f'https://swapi.dev/api/people/{i}')
+    for i in range(1, 37):
+        data = send_req(f'https://swapi.dev/api/starships/{i}')
         if data:
             data_list.append(data)
     return data_list
@@ -20,12 +18,14 @@ def save_data(data):
     with open('data.json', mode = 'w', encoding='utf-8') as file:
         json.dump(data, file)
 
+
 def update_data(data):
     data_dict = {}
     for index, el in enumerate(data):
-        el['homeworld'] = send_req(el['homeworld'])['name']
+        #el['homeworld'] = send_req(el['homeworld'])['name']
         data_dict[index] = el
     return data_dict
+
 
 def main():
     data = parse()
@@ -35,8 +35,4 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-
-
 
